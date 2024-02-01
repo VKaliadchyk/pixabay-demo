4e92b0ece76522b7e3493969d6cb51198082900c
Vladimir Kaliadchyk <vladimir.kaliadchyk@gmail.com>
2024-02-01 20:00:54 +0100

    Change field type in Resource.Failure, add nullability to 'fullHDURL' and 'imageURL' fields in PixabayImageInfo.kt

--- app/src/main/java/com/app/pixabaydemo/domain/entity/Resource.kt
+++ app/src/main/java/com/app/pixabaydemo/domain/entity/Resource.kt
@@ -4,5 +4,5 @@ sealed class Resource<T> {
 
     class Success<T>(val data: T) : Resource<T>()
 
-    class Failure<T>(val throwable: Throwable, val data: T? = null) : Resource<T>()
+    class Failure<T>(val message: String, val data: T? = null) : Resource<T>()
 }
