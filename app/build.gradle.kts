cdfb50e1b72af4c8d392a366ecf0a46bebdcd28b
Vladimir Kaliadchyk <vladimir.kaliadchyk@gmail.com>
2024-02-02 17:45:28 +0100

    Add Mockk dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,8 @@ dependencies {
     implementation("io.coil-kt:coil:2.5.0")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("io.mockk:mockk-android:1.13.9")
+    testImplementation("io.mockk:mockk-agent:1.13.9")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
